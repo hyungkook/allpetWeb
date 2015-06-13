7d8fbb71fe876651721d1aabd456f9f474653147
김형국 <hyungkook.kim@kt.com>
2015-06-13 14:38:37 +0900

    메뉴 정리중

--- app/ezamc/app.route-config.js
+++ app/ezamc/app.route-config.js
@@ -10,10 +10,34 @@
             .otherwise("/ezamc");
 
         $stateProvider
-          .state('ezamc', {
-            url: '/ezamc',
-            controller: 'ezamcHomeController',
-            templateUrl: 'view/indexHome.html'
-          });
-    }
+            .state('ezamc', {
+                url: '/ezamc',
+                controller: ezamcHomeController,
+                templateUrl: 'view/indexHome.html'
+            })
+            .state('homeInfo',{
+                url: '/homeInfo',
+                views: {
+                    '': {
+                        templateUrl: 'view/homeInfo/main.html',
+                        controller: homeInfoController
+                    },
+                    'menu@homeInfo': {
+                        templateUrl: 'view/homeInfo/leftMenu.html'
+                    }
+                }
+            })
+            .state('homeInfo.info',{
+                templateUrl: 'view/homeInfo/hospitalInfo.html',
+                controller: ezamcHomeController
+            })
+            .state('homeInfo.staff',{
+                templateUrl: 'view/homeInfo/hospitalStaff.html',
+                controller: ezamcHomeController
+            })
+            .state('homeInfo.equipment',{
+                templateUrl: 'view/homeInfo/hospitalEquipment.html',
+                controller: ezamcHomeController
+            });
+        }
 })();
---
+++ app/ezamc/view/homeInfo/homeInfoController.js
@@ -0,0 +1,8 @@
+angular.module('ezamc.home')
+    .controller('homeInfoController', homeInfoController);
+
+function homeInfoController($http, $scope, constant) {
+
+};
+
+
