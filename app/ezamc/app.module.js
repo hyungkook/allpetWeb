9936e53e9ac6ad178df1ae7b55590e00a27a7945
김형국 <hyungkook.kim@kt.com>
2015-06-13 19:31:46 +0900

    스테프 작업

--- app/ezamc/app.module.js
+++ app/ezamc/app.module.js
@@ -7,7 +7,7 @@ angular
     ])
     .constant("constant", {
         "imagePath": "http://14.63.174.249/getImages",
-        "contextPath": "http://localhost:8280/allpetapi/v1/",
-        //"contextPath": "http://14.63.174.249:8280/allpetapi/v1/",
+        //"contextPath": "http://localhost:8280/allpetapi/v1/",
+        "contextPath": "http://14.63.174.249:8280/allpetapi/v1/",
         "resourcePath": ""
     });
