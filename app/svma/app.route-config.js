910d780e59db876953005f0765f1b2ee66255340
hyungkook <sanaloveyou81@gmail.com>
2015-12-15 10:19:17 +0900

    서수회 작업

--- app/svma/app.route-config.js
+++ app/svma/app.route-config.js
@@ -21,7 +21,7 @@
                 templateUrl: 'view/login.html'
             })
             .state('companyInfo.loginPw', {
-                url: '/login/loginPw',
+                url: '/loginPw',
                 controller: LoginController,
                 templateUrl: 'view/loginPwReset.html'
             })
@@ -31,27 +31,27 @@
                 templateUrl: 'view/company/companyMain.html'
             })
             .state('companyInfo.company01', {
-                url: '/companyInfo/company01',
+                url: '/company01',
                 templateUrl: 'view/company/company01.html'
             })
             .state('companyInfo.company02', {
-                url: '/companyInfo/company02',
+                url: '/company02',
                 templateUrl: 'view/company/company02.html'
             })
             .state('companyInfo.company03', {
-                url: '/companyInfo/company03',
+                url: '/company03',
                 templateUrl: 'view/company/company03.html'
             })
             .state('companyInfo.company04', {
-                url: '/companyInfo/company04',
+                url: '/company04',
                 templateUrl: 'view/company/company04.html'
             })
             .state('companyInfo.company05', {
-                url: '/companyInfo/company05',
+                url: '/company05',
                 templateUrl: 'view/company/company05.html'
             })
             .state('companyInfo.company06', {
-                url: '/companyInfo/company06',
+                url: '/company06',
                 templateUrl: 'view/company/company06.html'
             })
             .state('boardInfo', {
@@ -60,15 +60,15 @@
                 templateUrl: 'view/board/boardMain.html'
             })
             .state('boardInfo.list01', {
-                url: '/companyInfo/list01',
+                url: '/list01/:boardType',
                 templateUrl: 'view/board/list01.html'
             })
             .state('boardInfo.list02', {
-                url: '/companyInfo/list02',
+                url: '/list02/:boardType',
                 templateUrl: 'view/board/list02.html'
             })
             .state('boardInfo.list03', {
-                url: '/companyInfo/list03',
+                url: '/list03/:boardType',
                 templateUrl: 'view/board/list03.html'
             })
             .state('reserveInfo', {
@@ -77,7 +77,7 @@
                 templateUrl: 'view/reserve/reserveMain.html'
             })
             .state('reserveInfo.list01', {
-                url: '/reserveInfo/list01',
+                url: '/list01',
                 templateUrl: 'view/reserve/list01.html'
             })
 
